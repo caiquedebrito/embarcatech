fb4e28c7de2b55f40911910d240db0addb80e401
Diego Farias de Freitas <e_dffreitas@uesc.net>
2024-12-18 10:11:27 -0300

    Feat: Adicionado Litro para Metro Cúbico e vice-versa

--- volume/volume.c
+++ volume/volume.c
@@ -1,12 +1,22 @@
 #include "volume.h"
 
 // Função para converter litro para mililitro
-double litro_para_mililitro(float litro) {
+float litro_para_mililitro(float litro) {
     return litro * 1000.0;
 }
 
 // Função para converter mililitro para litro
-double mililitro_para_litro(float mililitro) {
+float mililitro_para_litro(float mililitro) {
     return mililitro / 1000.0;
+}
+
+// Função para converter litro para metro cúbico
+float litro_para_metro_cubico(float litro) {
+    return litro / 1000.0;
+}
+
+// Função para converter metro cúbico para litro
+float metro_cubico_para_litro(float metro_cubico) {
+    return metro_cubico * 1000.0;
 }
 
