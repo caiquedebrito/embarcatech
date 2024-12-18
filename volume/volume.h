bc82538d41a064ae288a29b126631104060bd920
Diego Farias de Freitas <e_dffreitas@uesc.net>
2024-12-18 09:48:51 -0300

    Feat: Adicionado Conversão entre Litro e Mililitro

---
+++ volume/volume.h
@@ -0,0 +1,8 @@
+#ifndef CONVERSAO_H
+#define CONVERSAO_H
+
+// Declarações das funções de conversão
+double litro_para_mililitro(float litro);
+double mililitro_para_litro(float mililitro);
+
+#endif // CONVERSAO_H
