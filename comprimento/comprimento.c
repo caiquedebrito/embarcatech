f2111143c03e8b483af9e5595f8930059193edd0
Caique <caiquedebritoo@gmail.com>
2024-12-19 09:26:07 -0300

    Feat: Adicionado função para converter metros em centímetros

---
+++ comprimento/comprimento.c
@@ -0,0 +1,6 @@
+#include "comprimento.h"
+
+// Função para converter metros para centímetros
+float metro_para_centimetro(float metro) {
+    return metro * 100.0;
+}
---
+++ comprimento/comprimento.h
@@ -0,0 +1,7 @@
+#ifndef COMPRIMENTO_H
+#define COMPRIMENTO_H
+
+// Declarações das funções de comprimento
+float metro_para_centimetro(float metro);
+
+#endif // COMPRIMENTO_H
