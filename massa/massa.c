81efcd55c6c16ba7a64ac97ef4133c2c6e82e9e9
JotaPablo <142527513+JotaPablo@users.noreply.github.com>
2024-12-19 16:44:06 -0300

    Create massa.c

---
+++ massa/massa.c
@@ -0,0 +1,26 @@
+#include "massa.h"
+
+// Implementações das funções de conversão de massa
+float quilograma_para_grama(float quilograma) {
+    return quilograma * 1000.0;
+}
+
+float grama_para_quilograma(float grama) {
+    return grama / 1000.0;
+}
+
+float quilograma_para_tonelada(float quilograma) {
+    return quilograma / 1000.0;
+}
+
+float tonelada_para_quilograma(float tonelada) {
+    return tonelada * 1000.0;
+}
+
+float grama_para_tonelada(float grama) {
+    return grama / 1000000.0;
+}
+
+float tonelada_para_grama(float tonelada) {
+    return tonelada * 1000000.0;
+}
