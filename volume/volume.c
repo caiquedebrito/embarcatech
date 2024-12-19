cea98958928b20407db8bdc570ed0530f91e8484
Diego Farias de Freitas <diegofariass.028@gmail.com>
2024-12-19 07:47:26 -0300

    Feat: Adicionado Comentários para as funcionalidades

--- volume/volume.c
+++ volume/volume.c
@@ -1,31 +1,31 @@
-#include "volume.h"
+#include "volume.h" // Inclusão do cabeçalho para as funções de conversão de volume
 
-// Função para converter litro para mililitro
+// Função para converter litros para mililitros
 float litro_para_mililitro(float litro) {
-    return litro * 1000.0;
+    return litro * 1000.0; // Retorna o valor em mililitros
 }
 
-// Função para converter mililitro para litro
+// Função para converter mililitros para litros
 float mililitro_para_litro(float mililitro) {
-    return mililitro / 1000.0;
+    return mililitro / 1000.0; // Retorna o valor em litros
 }
 
-// Função para converter litro para metro cúbico
+// Função para converter litros para metros cúbicos
 float litro_para_metro_cubico(float litro) {
-    return litro / 1000.0;
+    return litro / 1000.0; // Retorna o valor em metros cúbicos
 }
 
-// Função para converter metro cúbico para litro
+// Função para converter metros cúbicos para litros
 float metro_cubico_para_litro(float metro_cubico) {
-    return metro_cubico * 1000.0;
+    return metro_cubico * 1000.0; // Retorna o valor em litros
 }
 
-// Função para converter mililitro para metro cúbico
+// Função para converter mililitros para metros cúbicos
 float mililitro_para_metro_cubico(float mililitro) {
-    return mililitro / 1000000.0;
+    return mililitro / 1000000.0; // Retorna o valor em metros cúbicos
 }
 
-// Função para converter metro cúbico para mililitro
+// Função para converter metros cúbicos para mililitros
 float metro_cubico_para_mililitro(float metro_cubico) {
-    return metro_cubico * 1000000.0;
+    return metro_cubico * 1000000.0; // Retorna o valor em mililitros
 }
