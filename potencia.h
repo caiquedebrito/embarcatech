045a2b90cafadb292e2088dfc08dede2ddc2294e
Dev1dH <devidh061@gmail.com>
2024-12-25 19:12:56 -0300

    Atualização das declarações das funções de conversão
    Atualização da declaração de duas novas funções para conversão de potência ( watts-quilowatts e watts-CV).

--- potencia.h
+++ potencia.h
@@ -2,6 +2,7 @@
 #define POTENCIA_H
 
 // Declarações da função de conversão
-float un_pot(float a); //converte de watt para hp
-
+float watt_para_hp(float watt); //converte de watt para hp
+float watt_para_kw(float watt); //converte de watts para quilowatts
+float watt_para_cv(float watt); //converte de watts para cv (cavalo-vapor)
 #endif // POTENCIA_H
