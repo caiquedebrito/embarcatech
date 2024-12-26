4a9212dd9df6b6cccdea6c1dff05da5794ba74c3
Devid H <devidh061@gmail.com>
2024-12-26 15:16:25 -0300

    changes

--- potencia.c
+++ potencia.c
@@ -1,8 +1,31 @@
 #include "potencia.h"
 
-float un_pot(float a) //converte de watts para hp
+float watt_para_hp(float watt) //converte de watts para hp
 {
-    float s;
-    s = a/746;
-    return s;
+    return watt/746;
+}
+
+float watt_para_kw(float watt)//converte de watts para quilowatts
+{
+    return watt/1000;
+}
+
+float watt_para_cv(float watt)//converte de watts para cv(cavalo-vapor)
+{
+    return watt/736;
+}
+
+float hp_para_watt(float hp) // converte de hp para watts
+{
+    return hp * 746;
+}
+
+float kw_para_watt(float kw) // converte de quilowatts para watts
+{   
+    return kw * 1000;
+}
+
+float cv_para_watt(float cv) // converte de cv para watts
+{    
+    return cv * 736;
 }
